fbae1a320baede3cc40d39366850c7faa9f3654e
FernandoNando2 <ferhdom@gmail.com>
2024-05-20 12:25:48 -0600

    Import the function instead of having it directly in this file

--- emotion_classifier.py
+++ emotion_classifier.py
@@ -1,34 +1,10 @@
 import joblib
-import soundfile
-import librosa
-import numpy as np
+from emotion_recognition import extract_feature
 import csv
 import os
 
 # Load the saved model
 model = joblib.load('7396.pkl')
-
-#DataFlair - Extract features (mfcc, chroma, mel) from a sound file
-def extract_feature(file_name, mfcc, chroma, mel):
-    with soundfile.SoundFile(file_name) as sound_file:
-        X = sound_file.read(dtype="float32")
-        sample_rate=sound_file.samplerate
-        if chroma:
-            stft=np.abs(librosa.stft(X))
-        result=np.array([])
-        if mfcc:
-            mfccs=np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T, axis=0)
-            mfccs = mfccs.flatten()
-            result=np.hstack((result, mfccs))
-        if chroma:
-            chroma=np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,axis=0)
-            chroma=chroma.flatten()
-            result=np.hstack((result, chroma))
-        if mel:
-            mel=np.mean(librosa.feature.melspectrogram(y=X, sr=sample_rate).T,axis=0)
-            mel=mel.flatten()
-            result=np.hstack((result, mel))
-    return result[:180]
 
 with open('classification_results.csv', 'w', newline='') as file:
     writer = csv.writer(file)
